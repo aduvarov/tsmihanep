b69901fd1bc3ae2356b56c59dcd500a5685910e2
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-12-05 17:19:41 +0500

    Enum

---
+++ 09-enums.ts
@@ -0,0 +1,53 @@
+enum ShapeKind {
+    Circle,
+    Square,
+}
+
+const myShape = ShapeKind.Circle
+
+interface Circle {
+    kind: ShapeKind.Circle
+    radius: number
+}
+
+interface Square {
+    kind: ShapeKind.Square
+    sideLength: number
+}
+
+const circle1: Circle = {
+    radius: 2,
+    kind: ShapeKind.Circle,
+}
+
+enum StatusCode {
+    ERROR = 500,
+    NOT_FOUND = 404,
+    NOT_AUTH = 403,
+}
+
+enum Grades {
+    Junior = 'junior',
+    Middle = 'middle',
+    Senior = 'senior',
+}
+
+interface Dev {
+    login: string
+    skills: string[]
+    level: Grades
+}
+
+const dev2: Dev = {
+    level: Grades.Junior,
+    login: 'anatoliy',
+    skills: ['HTML', 'CSS', 'JS'],
+}
+
+function upGrade(user: { level: Grades }) {
+    if (user.level === Grades.Junior) {
+        user.level = Grades.Middle
+    } else if (user.level === Grades.Middle) {
+        user.level = Grades.Senior
+    }
+}
